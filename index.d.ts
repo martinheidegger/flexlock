201b9ddbb383673edc1ecc49865494df2a6193ab
Martin Heidegger <martin.heidegger@gmail.com>
2018-02-06 02:41:34 +0900

    fixing index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,7 @@
+import createLock from './createLock'
+import createLocker from './createLocker'
+
 declare module 'flexlock' {
-  
+  export const createLock = createLock
+  export const createLocker = createLocker
 }
