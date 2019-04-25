141a108ab578fc0683f67cb5e5ecd06c085213b9
David Björklund <david.bjorklund@gmail.com>
2019-04-25 13:57:58 +0200

    Update createLocker.d.ts

--- createLocker.d.ts
+++ createLocker.d.ts
@@ -7,13 +7,13 @@ declare module "flexlock/createLocker" {
   interface FlexLock {
     (key: string): Promise<unlock>
     (key: string, timeout: number): Promise<unlock>
-    (key: string, process: Process): Promise<void>
-    (key: string, process: Process, timeout: number) : Promise<void>
+    (key: string, process: process): Promise<void>
+    (key: string, process: process, timeout: number) : Promise<void>
 
     released(): Promise<void>
     released(key: string): Promise<void>
-    released(onReleased: callback)
-    released(key: string, onReleased: callback)
+    released(onReleased: callback): void
+    released(key: string, onReleased: callback): void
   }
 
   export default function createLocker (onReleased?: callback): FlexLock
